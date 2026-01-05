bb5334f54dcc21b0b28b9124373caedb68024666
Aiden Pogue <dudarino3000@gmail.com>
2026-01-04 16:38:02 -0800

    feat: Implement MpdSongWrapper as ITagged

--- src/Mpd/MpdSongWrapper.cpp
+++ src/Mpd/MpdSongWrapper.cpp
@@ -1,19 +1,47 @@
 #include "MpdSongWrapper.hpp"
 
+#include <assert.h>
 #include <stdexcept>
 
-MpdSongWrapper::MpdSongWrapper(mpd_song *song) : song(song)
+MpdSongWrapper::MpdSongWrapper(mpd_song *song) : song(std::shared_ptr<mpd_song>(song, &mpd_song_free))
 {
-    if (song == nullptr)
-    {
-        throw std::runtime_error("Song cannot be null.");
-    }
+    assert(song);
 }
 
-MpdSongWrapper::~MpdSongWrapper()
+std::string MpdSongWrapper::GetSingleValue(mpd_tag_type key) const
 {
-    if (song != nullptr)
+    return mpd_song_get_tag(song.get(), key, 0);
+}
+
+std::vector<std::string> MpdSongWrapper::GetAllValues(mpd_tag_type key) const
+{
+    auto list = std::vector<std::string>();
+    //Most tags probably won't have more than 2 values.
+    list.reserve(2);
+
+    auto idx = 0;
+    auto rawValue = mpd_song_get_tag(song.get(), key, idx);
+
+    while (rawValue != nullptr)
     {
-        mpd_song_free(song);
+        list.emplace_back(rawValue);
+        rawValue = mpd_song_get_tag(song.get(), key, ++idx);
     }
+
+    return list;
+}
+
+unsigned MpdSongWrapper::GetId() const
+{
+    return mpd_song_get_id(song.get());
+}
+
+unsigned MpdSongWrapper::GetDurationMs() const
+{
+    return mpd_song_get_duration_ms(song.get());
+}
+
+std::string MpdSongWrapper::GetUri() const
+{
+    return mpd_song_get_uri(song.get());
 }
--- src/Mpd/MpdSongWrapper.hpp
+++ src/Mpd/MpdSongWrapper.hpp
@@ -1,16 +1,32 @@
-#include <mpd/song.h>
-
 #ifndef IM_MPD_MPDSONGWRAPPER_HPP
 #define IM_MPD_MPDSONGWRAPPER_HPP
 
+#include <mpd/song.h>
+#include "../TitleFormatting/ITagged.hpp"
+
 //Manages the lifetime of a mpd_song and provides some utility methods.
-class MpdSongWrapper
+class MpdSongWrapper : public ImpyD::TitleFormatting::ITagged
 {
-private:
-    mpd_song *song = nullptr;
+    std::shared_ptr<mpd_song> song = nullptr;
+
 public:
     MpdSongWrapper(mpd_song *song);
-    ~MpdSongWrapper();
+
+    MpdSongWrapper &operator=(MpdSongWrapper rhs)
+    {
+        song = std::move(rhs.song);
+        return *this;
+    }
+
+    [[nodiscard]] std::string GetSingleValue(mpd_tag_type key) const override;
+
+    [[nodiscard]] std::vector<std::string> GetAllValues(mpd_tag_type key) const override;
+
+    [[nodiscard]] unsigned GetId() const override;
+
+    [[nodiscard]] unsigned GetDurationMs() const override;
+
+    [[nodiscard]] std::string GetUri() const override;
 };
 
 
